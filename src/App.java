8bda760c883040c2a137642a9024726f09a32d1c
carlos <carlosaristi22@gmail.com>
2023-04-20 13:28:31 -0500

    Implementando json para manipulación

--- src/App.java
+++ src/App.java
@@ -19,11 +19,14 @@ public class App {
     //Encerramos todo el código de petición en un Try/Catch
     try {
       //Creamos variables para poder modificar el querry
-      String http = "https://serpapi.com/search.json?engine=google_scholar";
-      String query = "biology";
+      String http =
+        "https://serpapi.com/search.json?engine=google_scholar_author";
+      String authorid = "LSsXyncAAAAJ";
       String apiKey =
         "c7b692215b27d50b9842228c9ba707cecadcf67dc6b16ff41c24af002ed37618";
-      final String url = http + "&q=" + query + "&apikey=" + apiKey;
+      int num = 4;
+      final String url =
+        http + "&author_id=" + authorid + "&num=" + num + "&apikey=" + apiKey;
       //Inicio de la Conexion
       URL miUrl = new URL(url);
       URLConnection conexion = miUrl.openConnection();
